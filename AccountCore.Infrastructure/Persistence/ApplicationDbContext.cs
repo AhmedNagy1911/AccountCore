a685a5e0300cec1fa4da34873cce11442d3f5da0
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-02 03:02:30 +0300

    feat: configure ApplicationDbContext and entity mappings

---
+++ AccountCore.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AccountCore.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace AccountCore.Infrastructure.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser, ApplicationRole, string>(options)
+{
+
+    override protected void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ AccountCore.Infrastructure/Persistence/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AccountCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountCore.Infrastructure.Persistence.EntitiesConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder
+            .OwnsMany(x => x.RefreshTokens)
+            .ToTable("RefreshTokens")
+            .WithOwner()
+            .HasForeignKey("UserId");
+
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+
+    }
+}
