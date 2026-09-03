39cac9b7c5fad3d905e2745882f810b75567a1bc
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-03 06:05:25 +0300

    feat: add EmailService

---
+++ AccountCore.Infrastructure/Helpers/EmailBodyBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace AccountCore.Infrastructure.Helpers;
+
+public static class EmailBodyBuilder
+{
+    public static string GenerateEmailBody(string template, Dictionary<string, string> templateModel)
+    {
+        var templatePath = Path.Combine(
+           AppContext.BaseDirectory,
+           "Templates",
+           $"{template}.html");
+
+        //var streamReader = new StreamReader(templatePath);
+        //var body = streamReader.ReadToEnd();
+        //streamReader.Close();
+        var body = File.ReadAllText(templatePath);
+
+        foreach (var item in templateModel)
+            body = body.Replace(item.Key, item.Value);
+
+        return body;
+    }
+}
