f9a35336b410b88eaa44b661b6558b7f4d011d5b
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-06 05:45:15 +0300

    feat: Add Register endpoint and ResultExtensions

---
+++ AccountCore.API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿
+using AccountCore.Application.Contracts.Authentication;
+using AccountCore.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountCore.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthController(IAuthService authService, ILogger<AuthController> logger) : ControllerBase
+{
+    private readonly IAuthService _authservice = authService;
+    private readonly ILogger<AuthController> _logger = logger;
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _authservice.RegisterAsync(request, cancellationToken);
+
+        return result.IsSuccess ? Ok() : result.ToProblem();
+    }
+
+
+}
