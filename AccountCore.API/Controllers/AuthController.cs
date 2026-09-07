2ce2f8cf86bd3844aff22a92b84b5270ef678556
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-08 02:41:23 +0300

    feat: Add Refresh Token

--- AccountCore.API/Controllers/AuthController.cs
+++ AccountCore.API/Controllers/AuthController.cs
@@ -12,8 +12,15 @@ public class AuthController(IAuthService authService, ILogger<AuthController> lo
     private readonly IAuthService _authService = authService;
     private readonly ILogger<AuthController> _logger = logger;
 
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _authService.RegisterAsync(request, cancellationToken);
 
-    [HttpPost("")]
+        return result.IsSuccess ? Ok() : result.ToProblem();
+    }
+
+    [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginRequest request, CancellationToken cancellationToken = default)
     {
         _logger.LogInformation("Logging with email: {email} and password: {password}", request.Email, request.Password);
@@ -25,13 +32,14 @@ public class AuthController(IAuthService authService, ILogger<AuthController> lo
             : authResult.ToProblem();
     }
 
-
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterRequest request, CancellationToken cancellationToken)
+    [HttpPost("refresh")]
+    public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest request, CancellationToken cancellationToken = default)
     {
-        var result = await _authService.RegisterAsync(request, cancellationToken);
+        var authResult = await _authService.GetRefreshTokenAsync(request.Token, request.RefreshToken, cancellationToken);
 
-        return result.IsSuccess ? Ok() : result.ToProblem();
+        return authResult.IsSuccess
+            ? Ok(authResult.Value)
+            : authResult.ToProblem();
     }
 
 }
--- AccountCore.Application/Interfaces/IAuthService.cs
+++ AccountCore.Application/Interfaces/IAuthService.cs
@@ -6,6 +6,7 @@ namespace AccountCore.Application.Interfaces;
 
 public interface IAuthService
 {
-    Task<Result<AuthResponse>> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
     Task<Result> RegisterAsync(RegisterRequest request, CancellationToken cancellationToken = default);
+    Task<Result<AuthResponse>> GetTokenAsync(string email, string password, CancellationToken cancellationToken = default);
+    Task<Result<AuthResponse>> GetRefreshTokenAsync(string token, string refreshToken, CancellationToken cancellationToken = default);
 }
